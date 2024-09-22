4d5a7b43b243bdf05ca400ecffed2828983935c7
pyhumph <humphrey.david.dev@gmail.com>
2024-09-22 10:30:45 +0300

    add search bar with responsive design

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -5,8 +5,37 @@ class ShopPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('shop'),
+    return Column(
+      children: [
+        // search bar
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Search',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        )
+
+        //message
+
+        //hot picks
+      ],
     );
   }
 }
