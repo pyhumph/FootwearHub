03752bc351e1b0a8ec18808ee1cb73095de87384
pyhumph <humphrey.david.dev@gmail.com>
2024-09-24 01:53:19 +0300

    update cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,10 +1,51 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:footwearhub/components/cart_item.dart';
+import 'package:footwearhub/models/cart.dart';
+import 'package:footwearhub/models/shoe.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Cart'),);
+    return Consumer<Cart>(
+      builder: (context, value, child) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // heading
+            const Text(
+              'My Cart',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+
+            const SizedBox(
+              height: 10,
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.getShoeList().length,
+                itemBuilder: (context, index) {
+                  // get individual shoe
+                  Shoe individualShoe = value.getShoeList()[index];
+
+                  // return the cart item 
+                  return CartItem(shoe: individualShoe,);
+
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
